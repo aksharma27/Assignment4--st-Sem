c98806f89f0d87fc822d05f86016c91010283eac
Abhishek <aksharma.27mjm@gmail.com>
2022-01-26 20:44:42 +0530

    Q3 updated with actual answer

--- Q3.java
+++ Q3.java
@@ -7,11 +7,12 @@ public class Q3 {
         System.out.println("Enter the number of rows");
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        for (int i = 1; i<=N; i++){
-            for (int j =1; j<i;j++){
-                System.out.print(j);
-            }
-            System.out.println("\n");
+        String s = "";
+        for (int i =1; i<=N; i++){
+            s = s+i+s;
+            System.out.print(s);
+            System.out.println(" ");
         }
+
     }
 }
